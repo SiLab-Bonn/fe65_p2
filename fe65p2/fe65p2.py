87300bbd6c6ae8288b993c055c2e0c7bddfea685
coquelin77 <coquelin@physik.uni-bonn.de>
2017-11-22 10:40:25 +0100

    MAINT: formatting and comments

--- fe65p2/fe65p2.py
+++ fe65p2/fe65p2.py
@@ -12,11 +12,10 @@ import logging
 logging.getLogger().setLevel(logging.DEBUG)
 import os
 import numpy as np
-import time 
+import time
 import bitarray
 
 from numba import jit, njit
-
 
 
 @njit
@@ -25,13 +24,13 @@ def _interpret_raw_data(data, pix_data):
     prev_bcid = 0
     bcid = 0
     lv1id = 0
-    
+
     col = 0
     row = 0
     rowp = 0
     totB = 0
     totT = 0
-    
+
     for inx in range(data.shape[0]):
         if (data[inx] & 0x800000):
             bcid = data[inx] & 0x7fffff
@@ -42,17 +41,17 @@ def _interpret_raw_data(data, pix_data):
             prev_bcid = bcid
         else:
             col = (data[inx] & 0b111100000000000000000) >> 17
-            row = (data[inx] & 0b11111100000000000) >>11
+            row = (data[inx] & 0b11111100000000000) >> 11
             rowp = (data[inx] & 0b10000000000) >> 10
             totB = (data[inx] & 0b11110000) >> 4
             totT = (data[inx] & 0b1111)
-            
-            #print col, row, rowp, totT, totB
-            
+
+            # print col, row, rowp, totT, totB
+
             #| rowp = 1 | rowp = 0 |
             #| totL_T   | totR_T   |
             #| totL_B   | totR_B   |
-            
+
             if rowp == 1:
                 if(totT != 15):
                     pix_data[irec].bcid = bcid
@@ -60,8 +59,8 @@ def _interpret_raw_data(data, pix_data):
                     if row < 32:
                         pix_data[irec].row = (row % 32) * 2 + 1
                     else:
-                        pix_data[irec].row = 63 -((row % 32) * 2)
-                    pix_data[irec].col = col * 4 + (row / 32) * 2 
+                        pix_data[irec].row = 63 - ((row % 32) * 2)
+                    pix_data[irec].col = col * 4 + (row / 32) * 2
                     pix_data[irec].tot = totT
                     irec += 1
 
@@ -72,9 +71,9 @@ def _interpret_raw_data(data, pix_data):
                         pix_data[irec].row = (row % 32) * 2
                     else:
                         pix_data[irec].row = 63 - ((row % 32) * 2 + 1)
-                    
+
                     pix_data[irec].col = col * 4 + (row / 32) * 2
-                    
+
                     pix_data[irec].tot = totB
                     irec += 1
             else:
@@ -88,51 +87,53 @@ def _interpret_raw_data(data, pix_data):
                     pix_data[irec].col = col * 4 + (row / 32) * 2 + 1
                     pix_data[irec].tot = totT
                     irec += 1
-                    
+
                 if(totB != 15):
                     pix_data[irec].bcid = bcid
                     pix_data[irec].lv1id = lv1id
                     if row < 32:
                         pix_data[irec].row = (row % 32) * 2
                     else:
-                        pix_data[irec].row = 63 - ((row % 32) * 2 +1)
-                        
+                        pix_data[irec].row = 63 - ((row % 32) * 2 + 1)
+
                     pix_data[irec].col = col * 4 + (row / 32) * 2 + 1
                     pix_data[irec].tot = totB
                     irec += 1
-            
+
     return pix_data[:irec]
-    
+
+
 class fe65p2(Dut):
 
-    def __init__(self,conf=None):
-        
-        if conf==None:
-            conf = os.path.dirname(os.path.abspath(__file__)) + os.sep + "fe65p2.yaml"
-        
+    def __init__(self, conf=None):
+
+        if conf == None:
+            conf = os.path.dirname(os.path.abspath(
+                __file__)) + os.sep + "fe65p2.yaml"
+
         logging.info("Loading configuration file from %s" % conf)
-        
+
         conf = self._preprocess_conf(conf)
         super(fe65p2, self).__init__(conf)
-    
+
     def init(self):
         super(fe65p2, self).init()
 
     def _preprocess_conf(self, conf):
         return conf
-        
+
     def write_global(self):
-    
-        #size of global register
+
+        # size of global register
         self['global_conf'].set_size(145)
-        
+
         #write + start
         self['global_conf'].write()
-        
-        #wait for finish
+
+        # wait for finish
         while not self['global_conf'].is_ready:
             pass
-    
+
     def mask_sr(self, mask):
         """
         63 -> 124    63(127) -> 126  |  63(191) -> 0(128)     63(255) -> 2(130)
@@ -142,80 +143,80 @@ class fe65p2(Dut):
         2 -> 5       2(66)   -> 7    |  
         1 -> 0       1(65)   -> 2    |  1(129) -> 124(252)       1(193) -> 126(254)
         0 -> 1       0(64)   -> 3    |  0(128) -> 125(253)       0(192) -> 127(255)
-        
+
         """
-        conf_array_mcol = np.reshape(mask, (16,64*4))
-        mask = np.empty([16,64*4], dtype = np.bool)
-        
+        conf_array_mcol = np.reshape(mask, (16, 64 * 4))
+        mask = np.empty([16, 64 * 4], dtype=np.bool)
+
         for mcol in range(16):
             for i in range(256):
-                if( i<64 ):
-                    o =  (i - 1) * 2 + 3 * ((i + 1) % 2)  #(i - 1) * 2 #o = 1 + i/2
-                elif (i<128):
-                    o = (i - 64) * 2  + 3  * ((i - 64 + 1) % 2)
-                elif (i<192):
-                    o = 125 - (( (i-128)/2 ) * 4 + (i-128) % 2 ) + 128
+                if(i < 64):
+                    o = (i - 1) * 2 + 3 * ((i + 1) %
+                                           2)  # (i - 1) * 2 #o = 1 + i/2
+                elif (i < 128):
+                    o = (i - 64) * 2 + 3 * ((i - 64 + 1) % 2)
+                elif (i < 192):
+                    o = 125 - (((i - 128) / 2) * 4 + (i - 128) % 2) + 128
                 else:
-                    o = 127 - (( (i-192)/2 ) * 4 + (i-192) % 2 ) + 128
-                
-                mask[mcol][o] =  conf_array_mcol[mcol][i]
+                    o = 127 - (((i - 192) / 2) * 4 + (i - 192) % 2) + 128
 
+                mask[mcol][o] = conf_array_mcol[mcol][i]
 
-        mask_1d =  np.ravel(mask)
+        mask_1d = np.ravel(mask)
         lmask = mask_1d.tolist()
         bv_mask = bitarray.bitarray(lmask)
         return bv_mask
-            
-    def write_pixel(self, mask = None, ld = False):
-    
+
+    def write_pixel(self, mask=None, ld=False):
+
         if mask is not None:
             mask_gen = self.mask_sr(mask)
-            self['pixel_conf'][:]  = mask_gen
-                
+            self['pixel_conf'][:] = mask_gen
+
         #pixels in multi_column
-        self['pixel_conf'].set_size(16*4*64)
-    
-        #enable writing pixels
+        self['pixel_conf'].set_size(16 * 4 * 64)
+
+        # enable writing pixels
         self['control']['GATE_EN_PIX_SR'] = 1
         self['control'].write()
-        
+
         self['pixel_conf'].write()
-        
+
         while not self['pixel_conf'].is_ready:
             pass
-        
+
         self['control']['GATE_EN_PIX_SR'] = 0
         self['control'].write()
-        
+
         if(ld):
             self['control']['LD'] = 1
             self['control'].write()
             self['control']['LD'] = 0
             self['control'].write()
-        
-    def write_pixel_col(self, ld = False):
-    
+
+    def write_pixel_col(self, ld=False):
+
         #pixels in multi_column
-        self['pixel_conf'].set_size(4*64)
-    
-        #enable writing pixels
+        self['pixel_conf'].set_size(4 * 64)
+
+        # enable writing pixels
         self['control']['GATE_EN_PIX_SR'] = 1
         self['control'].write()
-        
-        self['pixel_conf'].write(4*64/8)
-        
+
+        self['pixel_conf'].write(4 * 64 / 8)
+
         while not self['pixel_conf'].is_ready:
             pass
-        
+
         self['control']['GATE_EN_PIX_SR'] = 0
         self['control'].write()
-        
+
         if(ld):
             self['control']['LD'] = 1
             self['control'].write()
             self['control']['LD'] = 0
             self['control'].write()
-    
+
     def write_en_mask(self, mask):
         self.write_pixel(mask)
         self['global_conf']['PixConfLd'] = 0b11
@@ -228,49 +229,51 @@ class fe65p2(Dut):
         self.write_global()
         self['global_conf']['InjEnLd'] = 0b0
 
-        
     def write_tune_mask(self, mask):
-            # 1  -> Sign = 1, TDac = 15 1111(lowest)
+            # 0  -> Sign = 1, TDac = 15 1111(lowest)
             # ...
             # 15 -> Sign = 1, TDac = 0  0000
-            # 16 -> Sign = 0, TDac = 0  0001
+            # 16 -> Sign = 0, TDac = 0  0000
             # ...
             # 31 -> Sign = 0, TDac = 15 1111
-            
-            mask_out = np.copy(mask)
-            mask_bits = np.unpackbits(mask_out)
-            mask_bits_array = np.reshape(mask_bits, (64,64,8))
-            mask_out[mask_bits_array[:,:,3] == 0] = 16 - mask_out[mask_bits_array[:,:,3] == 0]#15
-            #investigate here how to set 0 to 0
-            mask_bits = np.unpackbits(mask_out)
-            mask_bits_array = np.reshape(mask_bits, (64,64,8)).astype(np.bool)
-            mask_bits_array[:,:,3] = ~mask_bits_array[:,:,3]
-             
-            for bit in range(4):
-                mask_bits_sel = mask_bits_array[:,:,7-bit]
-                self.write_pixel(mask_bits_sel)
-                self['global_conf']['TDacLd'][bit] = 1
-                self.write_global()
-                self['global_conf']['TDacLd'][bit] = 0
-            
-            mask_bits_sel = mask_bits_array[:,:,3]
+
+        mask_out = np.copy(mask)
+        mask_bits = np.unpackbits(mask_out)
+        mask_bits_array = np.reshape(mask_bits, (64, 64, 8))
+        mask_out[mask_bits_array[:, :, 3] == 0] = 15 - \
+            mask_out[mask_bits_array[:, :, 3] == 0]  # 15
+        # investigate here how to set 0 to 0
+        mask_bits = np.unpackbits(mask_out)
+        mask_bits_array = np.reshape(mask_bits, (64, 64, 8)).astype(np.bool)
+        mask_bits_array[:, :, 3] = ~mask_bits_array[:, :, 3]
+
+        for bit in range(4):
+            mask_bits_sel = mask_bits_array[:, :, 7 - bit]
             self.write_pixel(mask_bits_sel)
-            self['global_conf']['SignLd'] = 1
+            self['global_conf']['TDacLd'][bit] = 1
             self.write_global()
-            self['global_conf']['SignLd'] = 0
-            
-    def interpret_raw_data(self, raw_data, meta_data = []):
-        data_type = {'names':['bcid','col','row','tot', 'lv1id','scan_param_id'], 'formats':['uint32','uint8','uint8','uint8','uint8', 'uint16']}
+            self['global_conf']['TDacLd'][bit] = 0
+
+        mask_bits_sel = mask_bits_array[:, :, 3]
+        self.write_pixel(mask_bits_sel)
+        self['global_conf']['SignLd'] = 1
+        self.write_global()
+        self['global_conf']['SignLd'] = 0
+
+    def interpret_raw_data(self, raw_data, meta_data=[]):
+        data_type = {'names': ['bcid', 'col', 'row', 'tot', 'lv1id', 'scan_param_id'], 'formats': [
+            'uint32', 'uint8', 'uint8', 'uint8', 'uint8', 'uint16']}
         ret = []
         if len(meta_data):
-            param, index = np.unique(meta_data['scan_param_id'], return_index=True)
+            param, index = np.unique(
+                meta_data['scan_param_id'], return_index=True)
             index = index[1:]
             index = np.append(index, meta_data.shape[0])
             index = index - 1
             stops = meta_data['index_stop'][index]
             split = np.split(raw_data, stops)
             for i in range(len(split[:-1])):
-                #print param[i], stops[i], len(split[i]), split[i]
+                # print param[i], stops[i], len(split[i]), split[i]
                 int_pix_data = self.interpret_raw_data(split[i])
                 int_pix_data['scan_param_id'][:] = param[i]
                 if len(ret):
@@ -281,16 +284,16 @@ class fe65p2(Dut):
             pix_data = np.recarray((raw_data.shape[0] * 2), dtype=data_type)
             ret = _interpret_raw_data(raw_data, pix_data)
         return ret
-                
+
     def power_up(self):
-    
+
         self['VDDA'].set_current_limit(200, unit='mA')
         self['VDDA'].set_voltage(1.2, unit='V')
         self['VDDA'].set_enable(True)
-        
+
         self['VDDD'].set_voltage(1.2, unit='V')
         self['VDDD'].set_enable(True)
-        
+
         self['VAUX'].set_voltage(1.2, unit='V')
         self['VAUX'].set_enable(True)
 
@@ -300,30 +303,30 @@ class fe65p2(Dut):
         self['VDDD'].set_enable(False)
         self['VAUX'].set_enable(False)
 
-
     def power_status(self):
         staus = {}
-       
-        staus['VDDD[V]'] =  self['VDDD'].get_voltage(unit='V')
+
+        staus['VDDD[V]'] = self['VDDD'].get_voltage(unit='V')
         staus['VDDD[mA]'] = self['VDDD'].get_current(unit='mA')
         staus['VDDA[V]'] = self['VDDA'].get_voltage(unit='V')
         staus['VDDA[mA]'] = self['VDDA'].get_current(unit='mA')
         staus['VAUX[V]'] = self['VAUX'].get_voltage(unit='V')
         staus['VAUX[mA]'] = self['VAUX'].get_current(unit='mA')
-        
+
         return staus
-    
+
     def dac_status(self):
         staus = {}
 
-        dac_names = ['PrmpVbpDac', 'vthin1Dac', 'vthin2Dac', 'vffDac', 'PrmpVbnFolDac', 'vbnLccDac', 'compVbnDac', 'preCompVbnDac']
-        for dac in  dac_names:
+        dac_names = ['PrmpVbpDac', 'vthin1Dac', 'vthin2Dac', 'vffDac',
+                     'PrmpVbnFolDac', 'vbnLccDac', 'compVbnDac', 'preCompVbnDac']
+        for dac in dac_names:
             staus[dac] = int(str(self['global_conf'][dac]), 2)
-        
+
         return staus
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     chip = fe65p2()
     chip.init()
     chip.power_up()
-    
--- fe65p2/scans/scan_controller.py
+++ fe65p2/scans/scan_controller.py
@@ -1,4 +1,7 @@
-from fe65p2.scans.noise_scan import NoiseScan
+''' Helper script to start scans
+'''
+
+from fe65p2.scans.noise_scan import NoiseTuning
 from fe65p2.scans.threshold_scan import ThresholdScan
 from fe65p2.scans.tu_threshold_scan import ThresholdScanTuned
 from fe65p2.scans.digital_scan import DigitalScan
@@ -7,7 +10,7 @@ from fe65p2.scans.timewalk_scan import TimewalkScan
 from fe65p2.scans.analog_scan import AnalogScan
 from fe65p2.scans.proofread_scan import proofread_scan
 from fe65p2.scan_base import ScanBase
-import fe65p2.plotting as  plotting
+import fe65p2.plotting as plotting
 from fe65p2.power import power
 import numpy as np
 import os
@@ -16,36 +19,37 @@ import yaml
 import time
 from itertools import cycle
 import logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(filename)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(filename)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
 
-fe65p2_path="/home/topcoup/Applications/fe65_p2"
-storage_dir="/media/topcoup/TB"
+fe65p2_path = "/home/topcoup/Applications/fe65_p2"
+storage_dir = "/media/topcoup/TB"
 
 par_conf = {
     "columns": [True] * 16,
-    #DAC parameters
+    # DAC parameters
     "PrmpVbpDac": 36,
     "vthin1Dac": 255,
     "vthin2Dac": 0,
-    "vffDac" : 42,          #not subject to change
-    "PrmpVbnFolDac" : 51,   #not subject to change
-    "vbnLccDac" : 1,        #not subject to change
-    "compVbnDac":25,        #not subject to change
-    "preCompVbnDac" : 110}   #critical, 50 for digi_scan_freq. 50 or 110 def. va
+    "vffDac": 42,  # not subject to change
+    "PrmpVbnFolDac": 51,  # not subject to change
+    "vbnLccDac": 1,  # not subject to change
+    "compVbnDac": 25,  # not subject to change
+    "preCompVbnDac": 110}  # critical, 50 for digi_scan_freq. 50 or 110 def. va
 
-#parameter folder name
-par_string = "Prmp"+str(par_conf['PrmpVbpDac']) +"_vthA"+str(par_conf['vthin1Dac'])+"_vthB"+str(par_conf['vthin2Dac'])\
-             +"_PreCmp"+str(par_conf['preCompVbnDac'])
+# parameter folder name
+par_string = "Prmp" + str(par_conf['PrmpVbpDac']) + "_vthA" + str(par_conf['vthin1Dac']) + "_vthB" + str(par_conf['vthin2Dac'])\
+             + "_PreCmp" + str(par_conf['preCompVbnDac'])
 
 
 def noise_sc():
     logging.info("Starting Noise Scan")
-    noise_sc = NoiseScan()
-    noise_mask_file = str(noise_sc.output_filename)+'.h5'
+    noise_sc = NoiseTuning()
+    noise_mask_file = str(noise_sc.output_filename) + '.h5'
 
     custom_conf = {
         "stop_pixel_count": 4,
-        "repeats" : 100 #100000
+        "repeats": 100  # 100000
     }
 
     scan_conf = dict(par_conf, **custom_conf)
@@ -55,23 +59,25 @@ def noise_sc():
     noise_sc.dut.close()
     return noise_mask_file
 
+
 def thresh_sc_unt(noise_mask_file=''):
     logging.info("Starting Threshold Scan")
     thrs_sc = ThresholdScan()
     custom_conf = {
         "mask_steps": 4,
         "repeat_command": 100,
-        "scan_range": [0.10, 0.7, 0.01], #[0.0, 0.6, 0.01],
+        "scan_range": [0.10, 0.7, 0.01],  # [0.0, 0.6, 0.01],
         "mask_filename": noise_mask_file,
-        "TDAC" : 16
+        "TDAC": 16
     }
 
     scan_conf = dict(par_conf, **custom_conf)
     thrs_sc.start(**scan_conf)
     thrs_sc.analyze()
-    thrs_mask_file = str(thrs_sc.output_filename)+'.h5'
+    thrs_mask_file = str(thrs_sc.output_filename) + '.h5'
     thrs_sc.dut.close()
     return thrs_mask_file
+
 
 def thresh_sc_tuned(noise_mask_file=''):
     logging.info("Starting Threshold Scan")
@@ -79,38 +85,42 @@ def thresh_sc_tuned(noise_mask_file=''):
     custom_conf = {
         "mask_steps": 4,
         "repeat_command": 100,
-        "scan_range": [0.005, 0.20, 0.005], #[0.0, 0.6, 0.01],
+        "scan_range": [0.005, 0.20, 0.005],  # [0.0, 0.6, 0.01],
         "mask_filename": noise_mask_file,
-        "TDAC" : 16
+        "TDAC": 16
     }
     scan_conf = dict(par_conf, **custom_conf)
     thrs_sc.start(**scan_conf)
     fetch_dic = thrs_sc.analyze()
-    thrs_mask_file=str(thrs_sc.output_filename)+'.h5'
+    thrs_mask_file = str(thrs_sc.output_filename) + '.h5'
     thrs_sc.dut.close()
     return thrs_mask_file, fetch_dic
+
 
 def timewalk_sc(noise_mask, th_mask, pix_list, mu, sigma):
     logging.info('receiving %s', mu)
     time_sc = TimewalkScan()
     range_min = 0.005
-    range_max = mu + 10.0*sigma
+    range_max = mu + 10.0 * sigma
     step = 0.005
-    logging.info('TIMEWALK mu, min, max, step: %f %f %f %f', mu,range_min,range_max, step)
+    logging.info('TIMEWALK mu, min, max, step: %f %f %f %f',
+                 mu, range_min, range_max, step)
     print 'TIMEWALK receiving ', mu, sigma
     custom_conf = {
-        "mask_steps" : 4,
-        "repeat_command" : 101,
-        "scan_range" : [range_min,range_max, step],
+        "mask_steps": 4,
+        "repeat_command": 101,
+        "scan_range": [range_min, range_max, step],
         #"scan_range":  [0.01, 0.25, 0.01],
-        "mask_filename":th_mask,
-        "pix_list":pix_list
+        "mask_filename": th_mask,
+        "pix_list": pix_list
     }
     scan_conf = dict(par_conf, **custom_conf)
-    scanrange=custom_conf["scan_range"]
+    scanrange = custom_conf["scan_range"]
     time_sc.start(**scan_conf)
-    time_sc.tdc_table(len(np.arange(scanrange[0], scanrange[1], scanrange[2]))+2)
+    time_sc.tdc_table(
+        len(np.arange(scanrange[0], scanrange[1], scanrange[2])) + 2)
     time_sc.dut.close()
+
 
 def digi_sc():
     logging.info("Starting Digital Scan")
@@ -125,11 +135,12 @@ def digi_sc():
     digital_sc.analyze()
     digital_sc.dut.close()
 
+
 def analog_sc():
     logging.info("Starting Analog Scan")
     ana_sc = AnalogScan()
     custom_conf = {
-        "mask_steps": 4*64,
+        "mask_steps": 4 * 64,
         "repeat_command": 100
     }
 
@@ -141,12 +152,12 @@ def analog_sc():
 
 def digi_shmoo_sc_cmd():
     digi_shmoo = DigitalScanFreq(None)
-    digi_shmoo.plots=False
+    digi_shmoo.plots = False
     custom_conf = {
 
         "mask_steps": 4,
         "repeat_command": 100,
-        "scan_type" : 'cmd'
+        "scan_type": 'cmd'
     }
     scan_conf = dict(par_conf, **custom_conf)
     digi_shmoo.scan(**scan_conf)
@@ -155,29 +166,31 @@ def digi_shmoo_sc_cmd():
 
 def digi_shmoo_sc_data():
     digi_shmoo = DigitalScanFreq(None)
-    digi_shmoo.plots=False
+    digi_shmoo.plots = False
     custom_conf = {
         "mask_steps": 4,
         "repeat_command": 100,
-        "scan_type" : 'data'
+        "scan_type": 'data'
     }
     scan_conf = dict(par_conf, **custom_conf)
     digi_shmoo.scan(**scan_conf)
     digi_shmoo.dut.close()
 
+
 def pix_reg_sc():
-    pix_reg = proofread_scan(fe65p2_path+"/fe65p2/fe65p2.yaml")
+    pix_reg = proofread_scan(fe65p2_path + "/fe65p2/fe65p2.yaml")
     pix_reg.scan(**par_conf)
     pix_reg.shmoo_plotting()
     pix_reg.dut.close()
-
 
 
 class status_sc(ScanBase):
     scan_id = "status_scan"
 
     def load_bit(self, bit):
-        if(bit==True): self.dut['intf']._sidev.DownloadXilinx(fe65p2_path+"/firmware/ise/fe65p2_mio.bit")
+        if(bit == True):
+            self.dut['intf']._sidev.DownloadXilinx(
+                fe65p2_path + "/firmware/ise/fe65p2_mio.bit")
         self.dut['VDDA'].set_current_limit(200, unit='mA')
         self.dut['VDDA'].set_voltage(1.2, unit='V')
         self.dut['VDDA'].set_enable(True)
@@ -197,14 +210,14 @@ class status_sc(ScanBase):
     def measure_temp(self):
         logging.info('Power Status: %s', str(self.dut.power_status()))
         logging.info('DAC Status: %s', str(self.dut.dac_status()))
-        N=1000
-        temp=0.0
+        N = 1000
+        temp = 0.0
         buf = []
         for m in range(0, N):
-            t=self.dut['ntc'].get_temperature('C')
+            t = self.dut['ntc'].get_temperature('C')
             buf.append(t)
             temp += t
-        temp_avg = temp/float(N)
+        temp_avg = temp / float(N)
         t_log = time.strftime("%d-%b-%H:%M:%S") + "\t" + str(temp_avg) + "\n"
         print t_log
         logname = 'reg_temp.dat'
@@ -217,29 +230,32 @@ class status_sc(ScanBase):
         self.dut.close()
         #n, bins, patches = plt.hist(buf, 8, facecolor='green', alpha=0.75)
         #plt.xlabel('Temperature (C)')
-        #plt.ylabel('Count')
+        # plt.ylabel('Count')
         #plt.title('ISRC 7')
         #plt.axis([22, 25, 0, N/2])
-        #plt.grid(True)
-        #plt.show()
-
-
+        # plt.grid(True)
+        # plt.show()
 
 
 def time_pixels(col):
     lp = []
-    if col==1: lp=[(2,6), (3,3)]
-    if col==2: lp=[(9,20),(14,10)]
-    if col==3: lp=[(20,20),(22,18)]
-    if col==4: lp=[(28,28),(30,31)]
-    if col==5: lp=[(36,36),(38,34)]
-    if col==6: lp=[(44,44),(44,12)]
-    if col==7: lp=[(48,49),(50,48)]
-    if col==8: lp=[(56,56),(58,58)]
+    if col == 1:
+        lp = [(2, 6), (3, 3)]
+    if col == 2:
+        lp = [(9, 20), (14, 10)]
+    if col == 3:
+        lp = [(20, 20), (22, 18)]
+    if col == 4:
+        lp = [(28, 28), (30, 31)]
+    if col == 5:
+        lp = [(36, 36), (38, 34)]
+    if col == 6:
+        lp = [(44, 44), (44, 12)]
+    if col == 7:
+        lp = [(48, 49), (50, 48)]
+    if col == 8:
+        lp = [(56, 56), (58, 58)]
     return lp
-
-
-
 
 
 if __name__ == "__main__":
@@ -251,10 +267,11 @@ if __name__ == "__main__":
     loadbit.measure_temp()
 
     #logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG)
-    for keys,values in par_conf.items():
+    for keys, values in par_conf.items():
         print keys, values
 
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
     os.chdir(storage_dir)
 
     working_dir = os.path.join(os.getcwd(), par_string)
@@ -262,9 +279,10 @@ if __name__ == "__main__":
         os.makedirs(working_dir)
     os.chdir(working_dir)
 
-    for c in cycle(range(0,2)): #goes on forever
-        #for just 1 iteration
-        if c==1: break
+    for c in cycle(range(0, 2)):  # goes on forever
+        # for just 1 iteration
+        if c == 1:
+            break
         '''
         #column independent scans
         time.sleep(1)
@@ -310,20 +328,19 @@ if __name__ == "__main__":
         #os.chdir('..')
         #time.sleep(1)
         '''
-        for i in range(1,9):   #1 to 9 for all columns
+        for i in range(1, 9):  # 1 to 9 for all columns
 
             pow.restart()
             loadbit2 = status_sc()
             loadbit2.load_bit(True)
             loadbit2.measure_temp()
 
-
-            cols = [False]*16
-            j=2*i-1
-            cols[j-1]=True
-            cols[j]=True
+            cols = [False] * 16
+            j = 2 * i - 1
+            cols[j - 1] = True
+            cols[j] = True
             par_conf['columns'] = cols
-            col_dir = os.path.join(os.getcwd(), "col"+str(i))
+            col_dir = os.path.join(os.getcwd(), "col" + str(i))
             if not os.path.exists(col_dir):
                 os.makedirs(col_dir)
             os.chdir(col_dir)
@@ -334,14 +351,13 @@ if __name__ == "__main__":
             time.sleep(1.0)
             thrs_mask, musigma = thresh_sc_tuned(noise_masks)
             time.sleep(1.0)
-            
-            #if i==1:
-            pixels = time_pixels(i)
-            timewalk_sc(thrs_mask, thrs_mask, pixels, musigma['mu'], musigma['sigma'])
 
-            #pow.restart()
+            # if i==1:
+            pixels = time_pixels(i)
+            timewalk_sc(thrs_mask, thrs_mask, pixels,
+                        musigma['mu'], musigma['sigma'])
+
+            # pow.restart()
             os.chdir('..')
 
-        par_conf['columns'] = [True]*16
-
-
+        par_conf['columns'] = [True] * 16
