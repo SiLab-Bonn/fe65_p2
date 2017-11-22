2f1dec0b787462bcf10d5d528181b27eff4269bd
coquelin77 <coquelin@physik.uni-bonn.de>
2017-11-22 10:41:09 +0100

    PRJ: rename

---
+++ fe65p2/scans/noise_tuning.py
@@ -0,0 +1,311 @@
+''' Threshold tuning based on electronic noise.
+    Tunes to the lowest possible threshold value. 
+'''
+
+from fe65p2.scan_base import ScanBase
+import fe65p2.plotting as plotting
+import fe65p2.DGC_plotting as DGC_plotting
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.backends.backend_pdf import PdfFile
+import matplotlib.pyplot as plt
+import time
+
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+
+import numpy as np
+import bitarray
+import tables as tb
+from bokeh.charts import output_file, hplot, save, show
+from bokeh.models.layouts import Column, Row
+from progressbar import ProgressBar
+import os
+
+local_configuration = {
+    "columns": [True] * 2 + [False] * 14,
+    "stop_pixel_count": 4,
+    "repeats": 100000,
+    # pars
+    "PrmpVbpDac": 36,
+    "vthin1Dac": 50,
+    "vthin2Dac": 0,
+    "vffDac": 24,
+    "PrmpVbnFolDac": 51,
+    "vbnLccDac": 1,
+    "compVbnDac": 25,
+    "preCompVbnDac": 110,
+
+}
+
+
+class NoiseTuning(ScanBase):
+    scan_id = "noise_tuning"
+
+    def __init__(self):
+        super(NoiseTuning, self).__init__()
+        self.vth1Dac = 0
+
+    def scan(self, stop_pixel_count=5, repeats=100000, **kwargs):
+        '''Scan loop
+        Parameters
+        ----------
+        mask : int
+            Number of mask steps.
+        repeat : int
+            Number of injections.
+        '''
+        logging.info('\e[31m Starting Noise Scan \e[0m')
+        INJ_LO = 0.2
+        self.dut['INJ_LO'].set_voltage(INJ_LO, unit='V')
+        self.dut['INJ_HI'].set_voltage(INJ_LO, unit='V')
+
+        self.dut['global_conf']['PrmpVbpDac'] = kwargs['PrmpVbpDac']
+        self.dut['global_conf']['vthin1Dac'] = kwargs['vthin1Dac']
+        self.dut['global_conf']['vthin2Dac'] = kwargs['vthin2Dac']
+        self.dut['global_conf']['vffDac'] = kwargs['vffDac']
+        self.dut['global_conf']['PrmpVbnFolDac'] = kwargs['PrmpVbnFolDac']
+        self.dut['global_conf']['vbnLccDac'] = kwargs['vbnLccDac']
+        self.dut['global_conf']['compVbnDac'] = kwargs['compVbnDac']
+        self.dut['global_conf']['preCompVbnDac'] = kwargs['preCompVbnDac']
+
+        self.dut['global_conf']['vthin1Dac'] = 255
+        self.dut['global_conf']['vthin2Dac'] = 0
+        self.dut['global_conf']['preCompVbnDac'] = 100
+        self.dut['global_conf']['PrmpVbpDac'] = 80
+
+        columns = kwargs['columns']
+
+        self.dut.write_global()
+        self.dut['control']['RESET'] = 0b01
+        self.dut['control']['DISABLE_LD'] = 0
+        self.dut['control']['PIX_D_CONF'] = 0
+        self.dut['control'].write()
+
+        self.dut['control']['CLK_OUT_GATE'] = 1
+        self.dut['control']['CLK_BX_GATE'] = 1
+        self.dut['control'].write()
+        time.sleep(0.01)
+
+        self.dut['control']['RESET'] = 0b11
+        self.dut['control'].write()
+
+        # write InjEnLd & PixConfLd to '1
+        self.dut['pixel_conf'].setall(True)
+        self.dut.write_pixel_col()
+        self.dut['global_conf']['SignLd'] = 1
+        self.dut['global_conf']['InjEnLd'] = 0
+        self.dut['global_conf']['TDacLd'] = 0b1111
+        self.dut['global_conf']['PixConfLd'] = 0b11
+        self.dut.write_global()
+
+        # write SignLd & TDacLd to '0
+        self.dut['pixel_conf'].setall(False)
+        self.dut.write_pixel_col()
+        self.dut['global_conf']['SignLd'] = 0
+        self.dut['global_conf']['InjEnLd'] = 1
+        self.dut['global_conf']['TDacLd'] = 0b1111
+        self.dut['global_conf']['PixConfLd'] = 0b11
+        self.dut.write_global()
+
+        # test hit
+        self.dut['global_conf']['TestHit'] = 0
+        self.dut['global_conf']['SignLd'] = 0
+        self.dut['global_conf']['InjEnLd'] = 0
+        self.dut['global_conf']['TDacLd'] = 0
+        self.dut['global_conf']['PixConfLd'] = 0
+
+        self.dut['global_conf']['OneSr'] = 1  # all multi columns in parallel
+        self.dut['global_conf']['ColEn'][:] = bitarray.bitarray(columns)
+        # self.dut['global_conf']['ColSrEn'][:] = bitarray.bitarray(columns)
+        self.dut.write_global()
+
+        #logging.info('Temperature: %s', str(self.dut['ntc'].get_temperature('C')))
+
+        mask_en = np.zeros([64, 64], dtype=np.bool)
+        #mask_tdac = np.ones([64, 64], dtype=np.uint8)
+        mask_tdac = np.full([64, 64], 16, dtype=np.uint8)
+
+        for inx, col in enumerate(columns):
+            if col:
+                mask_en[inx * 4:(inx + 1) * 4, :] = True
+
+        self.dut.write_en_mask(mask_en)
+
+        mask_tdac[:, :] = 0
+        self.dut.write_tune_mask(mask_tdac)
+
+        self.dut['global_conf']['TestHit'] = 0
+        self.dut['global_conf']['SignLd'] = 0
+        self.dut['global_conf']['InjEnLd'] = 0
+        self.dut['global_conf']['TDacLd'] = 0
+        self.dut['global_conf']['PixConfLd'] = 0
+        self.dut.write_global()
+
+        # exit()
+
+        # this seems to be working OK problem is probably bad injection on GPAC
+        self.dut['trigger'].set_delay(100)
+        self.dut['trigger'].set_width(1)  # try single
+        self.dut['trigger'].set_repeat(repeats)
+        self.dut['trigger'].set_en(False)
+
+        np.set_printoptions(linewidth=180)
+
+        idx = 0
+        finished = False
+
+        vthin1DacInc = 1
+        mask_disable_count = 0
+        iteration = 0
+
+        self.vth1Dac = kwargs['vthin1Dac']
+
+        while not finished:
+            with self.readout(scan_param_id=self.vth1Dac, fill_buffer=True, clear_buffer=True):
+                self.dut['global_conf']['vthin1Dac'] = self.vth1Dac
+                self.dut['global_conf']['vthin2Dac'] = kwargs['vthin2Dac']
+                self.dut['global_conf']['preCompVbnDac'] = kwargs['preCompVbnDac']
+                self.dut['global_conf']['PrmpVbpDac'] = kwargs['PrmpVbpDac']
+                self.dut.write_global()
+                time.sleep(0.1)
+
+                logging.info('Scan iteration: %d (vthin1Dac = %d)',
+                             iteration,  self.vth1Dac)
+                pbar = ProgressBar(maxval=10).start()
+
+                for i in range(10):
+
+                    self.dut['trigger'].start()
+
+                    pbar.update(i)
+
+                    while not self.dut['trigger'].is_done():
+                        pass
+
+            self.dut['global_conf']['vthin1Dac'] = 255
+            self.dut['global_conf']['vthin2Dac'] = 0
+            self.dut['global_conf']['preCompVbnDac'] = 50
+            self.dut['global_conf']['PrmpVbpDac'] = 80
+
+            self.dut.write_global()
+
+            dqdata = self.fifo_readout.data
+            data = np.concatenate([item[0] for item in dqdata])
+            hit_data = self.dut.interpret_raw_data(data)
+            hits = hit_data['col'].astype(np.uint16)
+            hits = hits * 64
+            hits = hits + hit_data['row']
+            value = np.bincount(hits)
+            value = np.pad(value, (0, 64 * 64 - value.shape[0]), 'constant')
+
+            # print mask_tdac[:8, :]
+            # print mask_en[:8, :]
+
+            logging.info(
+                'mean_tdac=' +
+                str(np.mean(mask_tdac[mask_en == True])) +
+                ' disabled=' + str(mask_disable_count)
+                + ' hist_tdac=' + str(np.bincount(mask_tdac[mask_en == True])))
+
+            nz = np.nonzero(value)
+
+            corrected = False
+            for i in nz[0]:
+                col = i / 64
+                row = i % 64
+
+                if mask_tdac[col, row] < 31:  # (val > 1?)
+                    mask_tdac[col, row] += 1
+                    corrected = True
+
+                if mask_tdac[col, row] == 31 and mask_en[col, row] == True:
+                    mask_en[col, row] = False
+                    mask_disable_count += 1
+
+                logging.debug('col=%d row=%d val=%d mask=%d', i /
+                              64, i % 64, value[i], mask_tdac[col, row])
+
+            # stop criteria:
+            # np.mean(mask_tdac[mask_en == True]) >=7
+            # if num disables is > 5% of chip/test area
+            if self.vth1Dac < 1 or np.mean(mask_tdac[mask_en == True]) >= 15 or mask_disable_count >= stop_pixel_count:
+                finished = True
+
+            # TODO: bin 16 doesnt get anything... all in bin 15
+
+            if not corrected:
+                self.vth1Dac -= vthin1DacInc
+
+            time.sleep(0.001)
+
+            self.dut.write_en_mask(mask_en)
+            self.dut.write_tune_mask(mask_tdac)
+
+            iteration += 1
+        self.dut['global_conf']['vthin1Dac'] = self.vth1Dac
+        self.dut['global_conf']['vthin2Dac'] = kwargs['vthin2Dac']
+        self.dut['global_conf']['preCompVbnDac'] = kwargs['preCompVbnDac']
+        self.dut['global_conf']['PrmpVbpDac'] = kwargs['PrmpVbpDac']
+
+        scan_results = self.h5_file.create_group(
+            "/", 'scan_results', 'Scan Results')
+        self.h5_file.create_carray(scan_results, 'tdac_mask', obj=mask_tdac)
+        self.h5_file.create_carray(scan_results, 'en_mask', obj=mask_en)
+        logging.info('Final vthin1Dac value: %s', str(self.vth1Dac))
+        self.final_vth1 = self.vth1Dac
+
+    def analyze(self):
+        h5_filename = self.output_filename + '.h5'
+        pdfName = self.output_filename + '.pdf'
+        pp = PdfPages(pdfName)
+        print pp
+
+        with tb.open_file(h5_filename, 'r+') as in_file_h5:
+            raw_data = in_file_h5.root.raw_data[:]
+            meta_data = in_file_h5.root.meta_data[:]
+
+            hit_data = self.dut.interpret_raw_data(raw_data, meta_data)
+            in_file_h5.create_table(
+                in_file_h5.root, 'hit_data', hit_data, filters=self.filter_tables)
+
+        status_plot = DGC_plotting.plot_status(h5_filename)
+        pp.savefig(status_plot)
+        occ_plot = DGC_plotting.plot_occupancy(h5_filename)
+        pp.savefig(occ_plot)
+        plt.clf()
+        tot_plot = DGC_plotting.plot_tot_dist(h5_filename)
+        pp.savefig(tot_plot)
+        plt.clf()
+        lv1id_plot = DGC_plotting.plot_lv1id_dist(h5_filename)
+        pp.savefig(lv1id_plot)
+        plt.clf()
+        '''
+        singlePixPolt, thresHM, thresVsPix, thresDist, noiseHM, noiseDist = DGC_plotting.scan_pix_hist(h5_filename)
+        pp.savefig(singlePixPolt)
+        plt.clf()
+        pp.savefig(thresHM)
+        plt.clf()
+        pp.savefig(thresVsPix)
+        plt.clf()
+        pp.savefig(thresDist)
+        plt.clf()
+        pp.savefig(noiseHM)
+        plt.clf()
+        pp.savefig(noiseDist)
+        plt.clf()
+        '''
+        t_dac_plot = DGC_plotting.t_dac_plot(h5_filename)
+        pp.savefig(t_dac_plot)
+        pp.close()
+
+        #output_file(self.output_filename + '.html', title=self.run_name)
+        #save(Column(Row(occ_plot, tot_plot), t_dac, status_plot))
+        # show(t_dac)
+
+
+if __name__ == "__main__":
+    scan = NoiseTuning()
+    scan.start(**local_configuration)
+    scan.analyze()
---
+++ fe65p2/scans/pixel_register_test.py
@@ -0,0 +1,258 @@
+'''
+	This scan writes a patterns of bits in the pixel registers
+	and reads them back. The occuring errors are counted and a
+	Shmoo plot is produced and printed on a .pdf
+	The supply voltage and the .bit file loaded can be changed
+	from here.
+	Global registers are checked as well and a second Shmoo
+	plot is printed in the same pdf.
+'''
+
+from fe65p2.scan_base import ScanBase
+import time
+import os
+import sys
+import bitarray
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.table import Table
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+local_configuration = {
+    "mask_steps": 4,
+    "columns": [True] * 16,
+    # DAC parameters
+    "PrmpVbpDac": 36,
+    "vthin1Dac": 255,
+    "vthin2Dac": 0,
+    "vffDac": 42,
+    "PrmpVbnFolDac": 51,
+    "vbnLccDac": 1,
+    "compVbnDac": 25,
+    "preCompVbnDac": 50
+}
+
+
+class proofread_scan(ScanBase):
+    scan_id = "proof_read_scan"
+
+    def __init__(self, dut_conf=None):
+        super(proofread_scan, self).__init__(dut_conf)
+
+    def scan(self, mask_steps=4, columns=[True] * 16, **kwargs):
+
+        # bitfiles = ["fe65p2_mio_40MHz.bit"]
+        #		voltages = [2.0]
+
+        self.dut['global_conf']['PrmpVbpDac'] = kwargs['PrmpVbpDac']
+        self.dut['global_conf']['vthin1Dac'] = kwargs['vthin1Dac']
+        self.dut['global_conf']['vthin2Dac'] = kwargs['vthin2Dac']
+        self.dut['global_conf']['vffDac'] = 42
+        self.dut['global_conf']['PrmpVbnFolDac'] = kwargs['PrmpVbnFolDac']
+        self.dut['global_conf']['vbnLccDac'] = kwargs['vbnLccDac']
+        self.dut['global_conf']['compVbnDac'] = kwargs['compVbnDac']
+        self.dut['global_conf']['preCompVbnDac'] = 50
+
+        #scan_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+        #path = scan_path.replace('fe65p2/scans','firmware/bits/goodSPI_bits/')
+        path = "/home/topcoup/Applications/fe65_p2/firmware/bits/goodSPI_bits/"
+        self.bitfiles = ["fe65p2_mio_3MHz.bit", "fe65p2_mio_4MHz.bit", "fe65p2_mio_6MHz.bit",
+                         "fe65p2_mio_8MHz.bit", "fe65p2_mio_12MHz.bit", "fe65p2_mio_16MHz.bit",
+                         "fe65p2_mio_24MHz.bit", "fe65p2_mio_32MHz.bit"]
+        self.voltages = [1.25, 1.2, 1.1, 1.0, 0.95, 0.90]
+
+        self.shmoo_errors = []
+        self.shmoo_global_errors = []
+
+        for bitfile in self.bitfiles:
+            logging.info("Loading " + bitfile)
+            setstatus = self.dut['intf']._sidev.DownloadXilinx(path + bitfile)
+            try:
+                setstatus == 0
+            except:
+                break
+
+            for volt in self.voltages:
+
+                self.dut['control']['RESET'] = 1
+                self.dut['control'].write()
+                self.dut['control']['RESET'] = 0
+                self.dut['control'].write()
+                # to change the supply voltage
+                self.dut['VDDA'].set_current_limit(200, unit='mA')
+                self.dut['VDDA'].set_voltage(volt, unit='V')
+                self.dut['VDDA'].set_enable(True)
+                self.dut['VDDD'].set_voltage(volt, unit='V')
+                self.dut['VDDD'].set_enable(True)
+                self.dut['VAUX'].set_voltage(1.25, unit='V')
+                self.dut['VAUX'].set_enable(True)
+                # global reg
+                self.dut['global_conf']['PrmpVbpDac'] = kwargs['PrmpVbpDac']
+                self.dut['global_conf']['vthin1Dac'] = kwargs['vthin1Dac']
+                self.dut['global_conf']['vthin2Dac'] = kwargs['vthin2Dac']
+                self.dut['global_conf']['vffDac'] = 42
+                self.dut['global_conf']['PrmpVbnFolDac'] = 51
+                self.dut['global_conf']['vbnLccDac'] = 51
+                self.dut['global_conf']['compVbnDac'] = kwargs['compVbnDac']
+                self.dut['global_conf']['preCompVbnDac'] = 50
+
+                self.dut['global_conf']['OneSr'] = 1
+                self.dut['global_conf']['SPARE'] = 0  # added by me, default 0
+                self.dut['global_conf']['ColEn'] = 0  # added by me, default 0
+                # added by me, default 15
+                self.dut['global_conf']['ColSrEn'] = 15
+                # added by me, default 0
+                self.dut['global_conf']['Latency'] = 400
+                self.dut['global_conf']['ColSrEn'].setall(
+                    True)  # enable programming of all columns
+
+                self.dut.write_global()
+                self.dut.write_global()  # need to write 2 times!
+
+                logging.info(self.dut.power_status())  # prints power supply
+
+                send = self.dut['global_conf'].tobytes()
+                rec = self.dut['global_conf'].get_data(size=19)
+                rec[18] = rec[18] & 0b1000000
+                glob_errors = [i for i in range(
+                    len(send)) if send[i] != rec[i]]
+                if (len(glob_errors) > 0):
+                    logging.warning("*** GLOBAL ERRORS " +
+                                    str(len(glob_errors)))
+                self.shmoo_global_errors.append(len(glob_errors))
+                #				for j in range(len(glob_errors)):
+                #					print "in position ", j, "value ", glob_errors[j]	#if you want to know where is the error
+
+                # pixel reg
+                self.dut['pixel_conf'][0] = 1
+                self.dut.write_pixel()
+                self.dut['control']['RESET'] = 0b11
+                self.dut['control'].write()
+
+                lmask = ([0] * (mask_steps - 1)) + [1]  # 1,0,0,0 pattern
+                lmask = lmask * ((64 * 64) / mask_steps + 1)
+                # 1,0,0,0 pattern for a total of 4096 bits
+                lmask = lmask[:64 * 64]
+                bv_mask = bitarray.bitarray(lmask)  # convert in binary
+                errors = []  # used for screen output - debug
+                ERR = []  # pixel errors storage
+                err_count = 0
+
+                logging.info('Temperature: %s', str(
+                    self.dut['ntc'].get_temperature('C')))
+
+                for i in range(0, 4):
+                    self.dut['pixel_conf'][:] = bv_mask
+                    self.dut.write_pixel()
+                    self.dut.write_pixel()
+                    time.sleep(0.5)
+                    returned_data = ''.join(format(x, '08b')
+                                            for x in self.dut['pixel_conf'].get_data())
+                    # the readout comes upside down
+                    returned_data_reversed = returned_data[::-1]
+
+                    pix_send = bv_mask
+                    pix_rec = bitarray.bitarray(returned_data_reversed)
+
+                    logging.debug('s ' + str(pix_send[:8]))
+                    logging.debug('r ' + str(pix_rec[:8]))
+
+                    errors.append([])
+                    for bit in xrange(len(pix_send)):
+                        if pix_send[bit] != pix_rec[bit]:
+                            errors[i].append(bit)
+                            ERR.append(bit)
+                            err_count += 1
+
+                    time.sleep(0.2)
+                    # shift the bit pattern
+                    bv_mask = bv_mask[1:] + bv_mask[:1]
+
+                self.shmoo_errors.append(err_count)
+
+                if len(errors[i]) > 0:
+                    logging.warning("*** PIXEL ERRORS  ***")
+                    for i in range(0, len(errors)):
+                        logging.warning("iteration " + str(i) + " errors " + str(
+                            len(errors[i])))  # , " at ", ' '.join([str(x) for x in errors[i]])
+
+    def shmoo_plotting(self):
+        ''' pixel register shmoo plot '''
+        plotname = "PixReg_" + str(time.strftime("%Y%m%d_%H%M%S_")) + ".pdf"
+        shmoopdf = PdfPages(plotname)
+        shmoonp = np.array(self.shmoo_errors)
+        data = shmoonp.reshape(len(self.voltages), -1, order='F')
+        fig, ax = plt.subplots()
+        plt.title('Pixel registers errors')
+        ax.set_axis_off()
+        fig.text(0.70, 0.05, 'SPI clock (MHz)', fontsize=14)
+        fig.text(0.02, 0.90, 'Supply voltage (V)', fontsize=14, rotation=90)
+        tb = Table(ax, bbox=[0.01, 0.01, 0.99, 0.99])
+        ncols = len(self.bitfiles)
+        nrows = len(self.voltages)
+        width, height = 1.0 / ncols, 1.0 / nrows
+        # Add cells
+        for (i, j), val in np.ndenumerate(data):
+            color = ''
+            if val == 0:
+                color = 'green'
+            if (val > 0 & val < 10):
+                color = 'yellow'
+            if val > 10:
+                color = 'red'
+            tb.add_cell(i, j, width, height, text=str(val),
+                        loc='center', facecolor=color)
+        # Row Labels...
+        for i in range(len(self.voltages)):
+            tb.add_cell(i, -1, width, height, text=str(self.voltages[i]) + 'V', loc='right',
+                        edgecolor='none', facecolor='none')
+        # Column Labels...
+        for j in range(len(self.bitfiles)):
+            freq_label = self.bitfiles[j].replace(
+                'fe65p2_mio_', '').replace('MHz.bit', '')
+            tb.add_cell(nrows + 1, j, width, height / 2, text=freq_label + ' MHz', loc='center',
+                        edgecolor='none', facecolor='none')
+        ax.add_table(tb)
+        shmoopdf.savefig()
+
+        ''' global register shmoo plot '''
+        shmoo_glob_np = np.array(self.shmoo_global_errors)
+        data_g = shmoo_glob_np.reshape(len(self.voltages), -1, order='F')
+        fig_g, ax_g = plt.subplots()
+        ax_g.set_axis_off()
+        fig_g.text(0.70, 0.05, 'SPI clock (MHz)', fontsize=14)
+        fig_g.text(0.02, 0.90, 'Supply voltage (V)', fontsize=14, rotation=90)
+        tb_g = Table(ax_g, bbox=[0.01, 0.01, 0.99, 0.99])
+        plt.title('Global registers errors')
+        # Add cells
+        for (i, j), val_g in np.ndenumerate(data_g):
+            color = ''
+            if val_g == 0:
+                color = 'green'
+            if val_g > 0:
+                color = 'red'
+            tb_g.add_cell(i, j, width, height, text=str(val_g),
+                          loc='center', facecolor=color)
+        # Row Labels...
+        for i in range(len(self.voltages)):
+            tb_g.add_cell(i, -1, width, height, text=str(self.voltages[i]) + 'V', loc='right',
+                          edgecolor='none', facecolor='none')
+        # Column Labels...
+        for j in range(len(self.bitfiles)):
+            freq_label = self.bitfiles[j].replace(
+                'fe65p2_mio_', '').replace('MHz.bit', '')
+            tb_g.add_cell(nrows + 1, j, width, height / 2, text=freq_label + ' MHz', loc='center',
+                          edgecolor='none', facecolor='none')
+        ax_g.add_table(tb_g)
+        shmoopdf.savefig()
+        shmoopdf.close()
+
+
+if __name__ == "__main__":
+    scan = proofread_scan(
+        "/home/user/Desktop/carlo/fe65_p2/fe65p2/fe65p2.yaml")
+    scan.start(**local_configuration)
+    scan.shmoo_plotting()
